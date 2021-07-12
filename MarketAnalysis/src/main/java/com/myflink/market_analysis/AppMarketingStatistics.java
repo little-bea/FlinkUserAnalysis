27c27eeba22c7ea4970c0d340e04f4c8d0fed563
xuzengfeng <1014361197@qq.com>
2021-07-12 11:27:25 +0800

    20210712

---
+++ MarketAnalysis/src/main/java/com/myflink/market_analysis/AppMarketingStatistics.java
@@ -0,0 +1,92 @@
+package com.myflink.market_analysis;
+
+import com.myflink.market_analysis.bean.ChannelPromotionCount;
+import com.myflink.market_analysis.bean.MarketingUserBehavior;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+import java.sql.Timestamp;
+
+/**
+ * @author xuzengfeng
+ * @description
+ */
+public class AppMarketingStatistics {
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        // 1. 从自定义数据源中读取数据
+        DataStream<MarketingUserBehavior> dataStream = env.addSource(new AppMarketingByChannel.SimulateMarketingUserBehaviorSource())
+                .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<MarketingUserBehavior>() {
+                    @Override
+                    public long extractAscendingTimestamp(MarketingUserBehavior element) {
+                        return element.getTimestamp();
+                    }
+                });
+
+        // 2. 开窗统计总量
+        SingleOutputStreamOperator<ChannelPromotionCount> resultStream = dataStream
+                .filter(data -> !"UNINSTALL".equals(data.getBehavior()))
+                .map(new MapFunction<MarketingUserBehavior, Tuple2<String, Long>>() {
+                    @Override
+                    public Tuple2<String, Long> map(MarketingUserBehavior marketingUserBehavior) throws Exception {
+                        return new Tuple2<>("total", 1L);
+                    }
+                })
+                .keyBy(0)
+                .timeWindow(Time.hours(1), Time.seconds(5))
+                .aggregate(new MarketingStatisticsAgg(), new MarketingStatisticsResult());
+
+        resultStream.print();
+
+        env.execute();
+    }
+
+    public static class MarketingStatisticsAgg implements AggregateFunction<Tuple2<String, Long>, Long, Long> {
+        @Override
+        public Long createAccumulator() {
+            return 0L;
+        }
+
+        @Override
+        public Long add(Tuple2<String, Long> value, Long accumulator) {
+            return accumulator + 1;
+        }
+
+        @Override
+        public Long getResult(Long accumulator) {
+            return accumulator;
+        }
+
+        @Override
+        public Long merge(Long a, Long b) {
+            return a + b;
+        }
+    }
+
+    public static class MarketingStatisticsResult implements WindowFunction<Long, ChannelPromotionCount, Tuple, TimeWindow> {
+        @Override
+        public void apply(Tuple tuple, TimeWindow window, Iterable<Long> input, Collector<ChannelPromotionCount> out) throws Exception {
+            String windowEnd = new Timestamp( window.getEnd() ).toString();
+            Long count = input.iterator().next();
+
+            out.collect(new ChannelPromotionCount("total", "total", windowEnd, count));
+        }
+    }
+
+}
